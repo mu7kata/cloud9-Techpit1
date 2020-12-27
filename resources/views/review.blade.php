57fec00591f73dd9b2b8dd457fe664f8431a2622
mu7kata <mu7kata@icloud.com>
2020-12-27 12:09:35 +0900

    レビュー登録機能項目変　#2更

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -21,15 +21,17 @@
         <div class="card">
             <div class="card-body">
               <div class="form-group">
-                <label>本のタイトル</label>
-                <input type='text' class='form-control' name='title' placeholder='タイトルを入力'>
+                <label>アイドルネーム</label>
+                <input type='text' class='form-control' name='title' placeholder='アイドルの名前を入力'>
+                <label>グループ</label>
+                <input type='text' class='form-control' name='group' placeholder='所属グループを入力'>
               </div>
               <div class="form-group">
               <label>レビュー本文</label>
                 <textarea class='description form-control' name='body' placeholder='本文を入力'></textarea>
               </div>
               <div class="form-group">
-                <label for="file1">本のノノノノのの</label>
+                <label for="file1">画像</label>
                 <input type="file" id="file1" name='image' class="form-control-file">
               </div>
               <input type='submit' class='btn btn-primary' value='レビューを登録'>
